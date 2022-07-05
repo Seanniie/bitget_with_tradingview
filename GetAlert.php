11137183f70887369cfdb88fcedc3e09fac45683
Seannie <shlim4915@gmail.com>
2022-07-06 00:33:32 +0800

    1

--- GetAlert.php
+++ GetAlert.php
@@ -1,6 +1,7 @@
 <?php 
     header("Content-Type: application/json; charset=utf-8");
 
+    
     $data =file_get_contents('php://input');
 
     #$data = '{"side":"longentry"}';
--- main_seannie.py
+++ main_seannie.py
@@ -10,7 +10,7 @@ import bitget.mix.plan_api as plan
 import bitget.mix.trace_api as trace
 import telegram
 
-bot = telegram.Bot(token='123:AAF3oscLU5z2rDzPFYd_oVe8eIjf7UqF-aA')
+bot = telegram.Bot(token='5465825333:AAF3oscLU5z2rDzPFYd_oVe8eIjf7UqF-aA')
 
 #Jun room
 chat_id = -1001634448671 
